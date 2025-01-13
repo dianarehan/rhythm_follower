476ae0d4c4e698a598bf9384da104fce6e9a8572
Pierre Emad <piereemorgan@gmail.com>
2025-01-13 20:38:21 +0200

    TextFeedback Done

--- Assets/Scripts/TextFeedback.cs
+++ Assets/Scripts/TextFeedback.cs
@@ -1,6 +1,66 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class TextFeedback
+public class TextFeedback : MonoBehaviour
 {
-    
+    // List 1: Positive/High Energy Words
+    List<string> positiveWords = new List<string>
+        {
+            "Epic",
+            "Lit",
+            "Fire",
+            "Winner",
+            "Vibing",
+            "Bet",
+            "Hard",
+            "Sick",
+            "Hell yeah"
+        };
+
+    // List 2: Negative/Low Energy Words
+    List<string> negativeWords = new List<string>
+        {
+            "Miss",
+            "Mid",
+            "Bruh",
+            "Meh",
+            "Womp",
+            "Loser"
+        };
+
+    TextMeshProUGUI feedbackText;
+    static int streak = 0;
+
+    private void Awake()
+    {
+        feedbackText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void ShowGoodMsg()
+    {
+        if (streak > 3)
+        {
+            feedbackText.text = "Combo x" + streak;
+        }
+        else
+        {
+            feedbackText.text = positiveWords[Random.Range(0, positiveWords.Count)];
+        }
+        gameObject.SetActive(false);
+        gameObject.SetActive(true);
+        
+        streak++;
+    }
+
+    public void ShowBadMsg()
+    {
+        feedbackText.text = negativeWords[Random.Range(0, negativeWords.Count)];
+        gameObject.SetActive(false);
+        gameObject.SetActive(true);
+
+        streak = 0;
+    }
+
+
 }
