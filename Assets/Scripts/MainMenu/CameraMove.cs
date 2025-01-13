4819f77288f0ce0a38088eb94f2b7f3c7c06142d
diana <daiana.rehan.dr@gmail.com>
2025-01-14 00:29:21 +0200

    main menu with a moving cameraaaa

--- Assets/Scripts/MainMenu/CameraMove.cs
+++ Assets/Scripts/MainMenu/CameraMove.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private Transform[] targets;
+    [SerializeField] private float lerpSpeed = 0.01f;
+
+    private int currentTargetIndex = 0;
+    private float lerpTime = 0.0f;
+
     void Start()
     {
-        
+        if (targets.Length > 0)
+        {
+            transform.position = targets[0].position;
+            transform.rotation = targets[0].rotation;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (targets.Length > 1)
+        {
+            lerpTime += Time.deltaTime * lerpSpeed;
+            transform.position = Vector3.Lerp(transform.position, targets[currentTargetIndex].position, lerpTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targets[currentTargetIndex].rotation, lerpTime);
+
+            if (lerpTime >= 1.0f)
+            {
+                lerpTime = 0.0f;
+                currentTargetIndex = (currentTargetIndex + 1) % targets.Length;
+            }
+        }
     }
 }
