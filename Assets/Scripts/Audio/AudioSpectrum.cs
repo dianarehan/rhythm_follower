af5f9572ae63dc49541c57e14ce13aa9450a2072
Pierre Emad <piereemorgan@gmail.com>
2025-01-14 22:58:44 +0200

    faked

--- Assets/Scripts/Audio/AudioSpectrum.cs
+++ Assets/Scripts/Audio/AudioSpectrum.cs
@@ -9,7 +9,10 @@ public class AudioSpectrum : MonoBehaviour
 
     void Update()
     {
-        AudioListener.GetSpectrumData(audioSpcetrum, 0, FFTWindow.Hamming);
+        // doesn't work on webGL
+        //AudioListener.GetSpectrumData(audioSpcetrum, 0, FFTWindow.Hamming);
+        
+        audioSpcetrum[0] = Random.Range(0.0f, 1.0f);
         if (audioSpcetrum != null && audioSpcetrum.Length > 0)
             spectrumValue = audioSpcetrum[0] * 100;
     }
